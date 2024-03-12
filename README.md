72d09aeafb6ae42c92488d93eabd35cf212ac241
WILbeerT-B <wilbertbundalian@gmail.com>
2024-03-11 23:23:47 -0700

    Update README, get input from player.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Project: Hangman - Ruby Course
 
 GOALS:
-Build a Hangman command line game where we can save and reload from a File.
+Build a Hangman command line game where one player play against the computer.
 
 ABOUT:
 Hangman is a guessing game for two or more players. One player thinks of a word, phrase, or sentence and the other(s) tries to guess it by suggesting letters or numbers within a certain number of guesses. Originally a paper-and-pencil game, there are now electronic versions.
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -6,5 +6,36 @@ my_lines = []
 for i in 0..lines.length-1 do
   my_lines.push(lines[i].chomp)
 end
+
+error_count = [1, 2, 3, 4, 5, 6, 7]
+error_chars = []
+
 secret_code = (my_lines.select {|line| line.length > 4}).sample
-puts "(1 2 3 4 5 6 7) #{secret_code.gsub(/[#{secret_code}]/, "_ ")} #{secret_code}" 
+print error_count 
+puts ""
+puts "#{secret_code.gsub(/[#{secret_code}]/, "_ ")} #{secret_code}(#{secret_code.length})"
+puts "Not matched: "
+
+def guess_right?(word, code)
+  word == code
+end
+
+input_match = []
+input_not_match = []
+
+
+while input_not_match.length < 7 do
+  puts "Please enter a letter: "
+  input = gets.chomp
+  
+  if secret_code.include? input
+    input_match.push(input)
+    break if guess_right?(input_match.join, secret_code)
+    
+  else
+    input_not_match.push(input)
+  end
+end
+
+puts "input matched: #{input_match.join}"
+puts "input not matched: #{input_not_match.join}"
