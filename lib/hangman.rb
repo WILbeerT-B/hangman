87d2bdcd130894994dbb9b7bf7b68a55837de442
WILbeerT-B <wilbertbundalian@gmail.com>
2024-03-20 22:51:36 -0700

    Reformat codes - add Board and Hangman classes to modularize codes.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,41 +1,57 @@
-puts "Hangman Game!"
-
-lines = File.readlines('google-10000-english-no-swears.txt')
-
-my_lines = []
-for i in 0..lines.length-1 do
-  my_lines.push(lines[i].chomp)
-end
-
-error_count = [1, 2, 3, 4, 5, 6, 7]
-error_chars = []
-
-secret_code = (my_lines.select {|line| line.length > 4}).sample
-print error_count 
-puts ""
-puts "#{secret_code.gsub(/[#{secret_code}]/, "_ ")} #{secret_code}(#{secret_code.length})"
-puts "Not matched: "
-
-def guess_right?(word, code)
-  word == code
-end
-
-input_match = []
-input_not_match = []
-
-
-while input_not_match.length < 7 do
-  puts "Please enter a letter: "
-  input = gets.chomp
-  
-  if secret_code.include? input
-    input_match.push(input)
-    break if guess_right?(input_match.join, secret_code)
+class Board
+  attr_reader :code
+  def initialize(code)
+    @code = Array.new(code.length, "_")
     
-  else
-    input_not_match.push(input)
+  end
+
+  def display_board
+    @code.each { |c| print c} 
+  end
+
+  def update_board(index, input)
+    @code[index] = input
   end
 end
 
-puts "input matched: #{input_match.join}"
-puts "input not matched: #{input_not_match.join}"
+class Hangman
+  def initialize
+    @lines = File.readlines('google-10000-english-no-swears.txt')
+    @my_lines = []
+    @my_lines = filter_lines
+    @secret_code = generate_code
+    @board = Board.new(@secret_code)
+    @count = 1
+  end
+
+  def filter_lines
+    @lines.each do |line|
+      @my_lines.push(line.chomp)
+    end
+  end
+
+  def generate_code
+    (@my_lines.select {|line| line.length > 4}).sample
+  end
+
+  def play
+    puts "Hangman"
+    puts @secret_code
+    @board.display_board
+    while @count < @secret_code.length
+      player_input
+    end
+  end
+
+  def player_input
+    puts
+    print "Enter a letter: "
+    input = gets.chomp
+    idx = @secret_code.index(input)
+    @board.update_board(idx, input)
+    @board.display_board
+  end
+end
+
+game = Hangman.new
+game.play
