2ab62882af6c5d45454c1cac364466a0532ff446
WILbeerT-B <wilbertbundalian@gmail.com>
2024-03-25 06:07:40 -0700

    Add get methods, update filter_lines method
    Add attr_readers in Hangman class, update filter_lines
    method to return my_lines array in generate_code method.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -6,39 +6,46 @@ class Board
   end
 
   def display_board
-    @code.each { |c| print c} 
+    code.each { |c| print c} 
   end
 
   def update_board(index, input)
-    @code[index] = input
+    code[index] = input
   end
 end
 
 class Hangman
+  attr_reader :lines, :my_lines, :secret_code, :board, :match, :not_match
   def initialize
     @lines = File.readlines('google-10000-english-no-swears.txt')
     @my_lines = []
     @my_lines = filter_lines
     @secret_code = generate_code
     @board = Board.new(@secret_code)
-    @count = 1
+    @match = []
+    @not_match = []
   end
 
   def filter_lines
-    @lines.each do |line|
-      @my_lines.push(line.chomp)
+    for i in 0..lines.length-1 do
+      my_lines.push(lines[i].chomp)
     end
+    my_lines
+    #for line in lines do 
+      #my_lines.push(line.chomp)  
+    #end
   end
 
   def generate_code
-    (@my_lines.select {|line| line.length > 4}).sample
+    (my_lines.select {|line| line.length > 4}).sample
   end
 
   def play
     puts "Hangman"
-    puts @secret_code
-    @board.display_board
-    while @count < @secret_code.length
+    p secret_code
+    puts secret_code.length
+    board.display_board
+    while not_match.length < 7
       player_input
     end
   end
@@ -47,9 +54,19 @@ class Hangman
     puts
     print "Enter a letter: "
     input = gets.chomp
-    idx = @secret_code.index(input)
-    @board.update_board(idx, input)
-    @board.display_board
+    idx = secret_code.index(input)
+    if idx != nil
+      match << input
+      board.update_board(idx, input)
+    else
+      not_match << input
+    end
+    
+    board.display_board
+    puts
+    puts "Not match: #{@not_match}"
+    puts "Match: #{@match}"
+    puts "Game over! You did not guess the secret code." if @not_match.length == 7
   end
 end
 
