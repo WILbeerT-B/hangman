f30e7aa24d52a69fe75a8401642cccc1cb12733d
WILbeerT-B <wilbertbundalian@gmail.com>
2024-03-29 05:57:35 -0700

    Add var count to check the length of incorrect letters

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -25,10 +25,11 @@ class Hangman
     @board = Board.new(@secret_code)
     @match = []
     @not_match = []
+    @count = 0
   end
 
   def filter_lines
-    for i in 0..lines.length-1 do
+    for i in 0...lines.length do
       my_lines.push(lines[i].chomp)
     end
     my_lines
@@ -46,18 +47,21 @@ class Hangman
     p secret_code
     puts secret_code.length
     board.display_board
-    while not_match.length < 7
+    while @count != 7 do
       player_input
     end
   end
 
   def player_input
     puts
+    puts
     print "Enter a letter: "
     input = gets.chomp
     idx = secret_code.index(input)
     if secret_code.count(input) == 0
+      @count += 1
       not_match << input
+      
     elsif secret_code.count(input) == 1
       match << input
       board.update_board(idx, input)
@@ -69,7 +73,7 @@ class Hangman
         board.update_board(all_index[i], input)
       end
     end
-    
+    puts 
     board.display_board
     puts
     puts "Not match: #{not_match}"
@@ -77,10 +81,10 @@ class Hangman
     puts "Board code: #{board.code.join}"
     puts "Game over! You did not guess the secret code '#{secret_code}'." if not_match.length == 7
     if board.code.join == secret_code
-      not_match.length == 7
+      @count = 7
       puts "You guess it right!"
-      
     end
+    puts @count
   end
 end
 
